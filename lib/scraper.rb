3d5d1e978e261db7b16d667902d71c31b97b9f4b
Nick Solonyy <nicksolony@gmail.com>
2020-01-15 01:02:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -46,18 +46,7 @@ class Scraper
     :bio
     }
     
-    doc.css("div.student-card").each { |student| 
-    
-      name = student.css("h4.student-name").text
-      location = student.css("p.student-location").text
-      profile_url = student.css("a").attribute("href").text
-      student_id = student.attribute("id").text.to_sym
-      students << { 
-        :name => name,
-        :location=> location,
-        :profile_url => profile_url
-      }
-  end
+   end
 
 end
 
