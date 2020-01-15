371cf50199275a1d9451a2200fb0508a31476ffe
Nick Solonyy <nicksolony@gmail.com>
2020-01-15 03:42:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,6 +12,8 @@ class Student
 
   def self.create_from_collection(students_array)
     
+    students_array.each {|student| self.new(student)
+    
   end
 
   def add_student_attributes(attributes_hash)
