0f188c6dd4ddcc2a1ed4de6d71e7be006d8fa3c0
Nick Solonyy <nicksolony@gmail.com>
2020-01-15 00:56:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -35,8 +35,8 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
      doc = Nokogiri::HTML(open(profile_url))
+    student = 
     
-    students = []
     
     doc.css("div.student-card").each { |student| 
     
