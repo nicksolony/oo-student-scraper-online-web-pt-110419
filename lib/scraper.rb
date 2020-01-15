dc9892de5ea4703b4abf4d47ead84d8fcac53e92
Nick Solonyy <nicksolony@gmail.com>
2020-01-15 00:52:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,36 @@
 require 'open-uri'
 require 'pry'
+#require 'nokogiri'
+
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    doc = Nokogiri::HTML(open(index_url))
     
+    students = []
+    
+    doc.css("div.student-card").each { |student| 
+    
+      name = student.css("h4.student-name").text
+      location = student.css("p.student-location").text
+      profile_url = student.css("a").attribute("href").text
+      student_id = student.attribute("id").text.to_sym
+      students << { 
+        :name => name,
+        :location=> location,
+        :profile_url => profile_url
+      }
+    
+    #  students[student_id.to_sym] = {
+    #    :name => student.css("h4.student-name").text,
+    #    :location => student.css("p.student-location").text,
+    #    :profile_url => student.css("a").attribute("href").text      
+    #  }.to_a
+    }
+
+    students
+  #  binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
@@ -13,3 +39,7 @@ class Scraper
 
 end
 
+# student_id:  student.attribute("id").text.split("-card")
+# name: student.css("h4.student-name").text
+# location: student.css("p.student-location").text
+# profile_url: student.css("a").attribute("href").text
