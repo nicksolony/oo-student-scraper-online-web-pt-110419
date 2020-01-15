9c51ef717ecaaf4c4b541af96c3baa1695f6486e
Nick Solonyy <nicksolony@gmail.com>
2020-01-15 02:11:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -73,6 +73,8 @@ class Scraper
       github = xml.css("a").attribute("href").text
     when "rss-icon.png"
       blog = xml.css("a").attribute("href").text
+    else
+      nil
     end
     }
     
